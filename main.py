fc2e146d66c9fef7cdc7a7dad2adb5d862bd2eb1
Phillip <phillipables1@gmail.com>
2018-02-22 08:24:08 -0500

    name age number and till one hundred input vars and a for loop to concat it all together

---
+++ main.py
@@ -0,0 +1,11 @@
+  # http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+name = input("What is your name? ")
+age = input("What is your age? ")
+number = input("Time is precious " + name + ", Enter a number: ")
+tillOneHundred = 100 - int(age)
+for x in range(0, int(number)):
+    print(name + ", you have "+ str(tillOneHundred) + " years until you are ONE HUNDRED")
+
+  # The only tricky thing here was having to cast everythings type
+  # input as a string but needs an int type
+  # output as an int but need a str type
